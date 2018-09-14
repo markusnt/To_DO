d9c220fef84ae446b3f94957f6288b0718f2b44e
MarkusNt <markus_nathan@live.com>
2018-09-14 15:22:11 -0300

    test

---
+++ js/helpers.js
@@ -0,0 +1,45 @@
+(function (window) {
+	'use strict';
+
+	// Get element(s) by CSS selector:
+	window.qs = function (selector, scope) {
+		return (scope || document).querySelector(selector);
+	};
+	window.qsa = function (selector, scope) {
+		return (scope || document).querySelectorAll(selector);
+	};
+
+	// addEventListener wrapper:
+	window.$on = function (target, type, callback, useCapture) {
+		target.addEventListener(type, callback, !!useCapture);
+	};
+
+	window.$delegate = function (target, selector, type, handler) {
+		function dispatchEvent(event) {
+			var targetElement = event.target;
+			var potentialElements = window.qsa(selector, target);
+			var hasMatch = Array.prototype.indexOf.call(potentialElements, targetElement) >= 0;
+
+			if (hasMatch) {
+				handler.call(targetElement, event);
+			}
+		}
+
+		var useCapture = type === 'blur' || type === 'focus';
+
+		window.$on(target, type, dispatchEvent, useCapture);
+	};
+
+
+	window.$parent = function (element, tagName) {
+		if (!element.parentNode) {
+			return;
+		}
+		if (element.parentNode.tagName.toLowerCase() === tagName.toLowerCase()) {
+			return element.parentNode;
+		}
+		return window.$parent(element.parentNode, tagName);
+	};
+
+	NodeList.prototype.forEach = Array.prototype.forEach;
+})(window);
